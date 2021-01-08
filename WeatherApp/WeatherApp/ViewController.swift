9ac018c8ca024bab793edc597496f94d850ec169
Viktor Rubenko <vrubenko@gmail.com>
2021-01-08 03:45:08 +0300

    Add OW steps to models, erase models before addings

--- WeatherApp/WeatherApp/ViewController.swift
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBOutlet var table: UITableView!
     
-    var models = [Weather]()
+    var models = [Step]()
     
     let locationManager = CLLocationManager()
     
@@ -83,8 +83,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             URLSession.shared.dataTask(with: url) {data, response, error in
                 if let data = data {
                     do {
+                        // Clear models if user update forecast
+                        if !self.models.isEmpty {
+                            self.models.removeAll()
+                        }
                         let result = try JSONDecoder().decode(Response.self, from: data)
-                        print(result)
+                        for step in result.list {
+                            self.models.append(step)
+                        }
                     } catch let error {
                         print(error)
                     }
@@ -97,66 +103,73 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
 }
 
-struct Weather {
-    
-}
 
 struct Response: Codable {
     let cod: String
     let message: Int
     let cnt: Int
     let list: [Step]
+    }
+
+struct Step: Codable {
+    let dt: Int // UTC time
+    let main: MainInfo
+    let weather: [Weather]
+    let clouds: Clouds
+    let wind: Wind
+    let visibility: Int
+    let pop: Float
+    let rain: Rain?
+    let sys: Sys
+    let dt_txt: String
     
-    struct Step: Codable {
-        let dt: Int
-        let main: MainInfo
-        let weather: [Weather]
-        let clouds: Clouds
-        let wind: Wind
-        let visibility: Int
-        let pop: Float
-        let rain: Rain?
-        let sys: Sys
-        let dt_txt: String
-        
-        struct MainInfo: Codable {
-            let temp: Float
-            let feels_like: Float
-            let temp_min: Float
-            let temp_max: Float
-            let pressure: Float
-            let sea_level: Float
-            let grnd_level: Float
-            let humidity: Float
-            let temp_kf: Float
+    struct MainInfo: Codable {
+        let temp: Float
+        let feels_like: Float
+        let temp_min: Float
+        let temp_max: Float
+        let pressure: Float
+        let sea_level: Float
+        let grnd_level: Float
+        let humidity: Float
+        let temp_kf: Float
+    }
+    
+    struct Weather: Codable {
+        /*
+         id:
+            2XX - Thunderstorm
+            3XX - Drizzle
+            5XX - Rain
+            6XX - Snow
+            7XX - Atmospere
+            800 - Clear
+            80X - Clouds
+         */
+        let id: Int
+        let main: String
+        let description: String
+        let icon: String
+    }
+    
+    struct Clouds: Codable {
+        let all: Int
+    }
+    
+    struct Wind: Codable {
+        let speed: Float
+        let deg: Int
+    }
+    
+    struct Rain: Codable {
+        let th: Float
+        enum CodingKeys: String, CodingKey {
+            case th = "3h"
         }
-        
-        struct Weather: Codable {
-            let id: Int
-            let main: String
-            let description: String
-            let icon: String
-        }
-        
-        struct Clouds: Codable {
-            let all: Int
-        }
-        
-        struct Wind: Codable {
-            let speed: Float
-            let deg: Int
-        }
-        
-        struct Rain: Codable {
-            let th: Float
-            enum CodingKeys: String, CodingKey {
-                case th = "3h"
-            }
-        }
-        
-        struct Sys: Codable {
-            let pod: String
-        }
+    }
+    
+    struct Sys: Codable {
+        let pod: String
     }
     
     
