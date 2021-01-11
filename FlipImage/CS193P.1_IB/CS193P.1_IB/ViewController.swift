35d6e0074bf74d1e497ff8ab2e41013036bb8374
Viktor Rubenko <vrubenko@gmail.com>
2021-01-11 19:01:50 +0300

    Add emoji button with flip action

--- FlipImage/CS193P.1_IB/CS193P.1_IB/ViewController.swift
+++ FlipImage/CS193P.1_IB/CS193P.1_IB/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    
+    @IBOutlet var touchCard: UIButton!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "Cow", on: touchCard)
     }
-
-
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor =  #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor =  #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
 }
-
