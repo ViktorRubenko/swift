018d3e43d43c23feea2cfd6358e4d710eebb28ef
Viktor Rubenko <vrubenko@gmail.com>
2021-01-11 18:26:26 +0300

    init

---
+++ FlipImage/CS193P.1_IB/CS193P.1_IB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CS193P.1_IB
+//
+//  Created by Victor Rubenko on 11.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
