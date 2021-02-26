dab7eb188131640f64464d70c133892fc6963138
Viktor Rubenko <vrubenko@gmail.com>
2021-02-26 17:03:12 +0300

    p3 is done

--- 100_days/project3/StormViewer/DetailVIewController.swift
+++ 100_days/project3/StormViewer/DetailVIewController.swift
@@ -22,11 +22,37 @@ class DetailViewController: UIViewController, UIScrollViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         if let selectedImage = selectedImage {
+            title = selectedImage
+            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
             imageView.image = UIImage(named: selectedImage)
             imageView.sizeToFit()
             scrollView?.contentSize = imageView.frame.size
         }
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("no image")
+            return
+        }
+        
+
+        if let selectedImage = selectedImage {
+        let vc = UIActivityViewController(activityItems: [selectedImage, image], applicationActivities: [])
+        //vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true, completion: nil)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
     }
     
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
