f3718cef160b1bb1a014b7d055d5f1f5fcc6f2ff
Viktor Rubenko <vrubenko@gmail.com>
2021-02-28 00:13:25 +0300

    add WKWebView

--- 100_days/project4/project4/ViewController.swift
+++ 100_days/project4/project4/ViewController.swift
@@ -6,12 +6,45 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let url = URL(string: "https://pals-geo.ru")
+        webView.load(URLRequest(url: url!))
+        webView.allowsBackForwardNavigationGestures = true
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "google.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "ya.com", style: .default, handler: openPage))
+        self.present(ac, animated: true, completion: nil)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)
+        webView.load(URLRequest(url: url!))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
+    override func loadView() {
+        super.loadView()
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
     }
 
 
