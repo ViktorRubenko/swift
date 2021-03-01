a08714dac9eb27213a8559ba91135b6a2fbb5a13
Viktor Rubenko <vrubenko@gmail.com>
2021-03-01 21:53:39 +0300

    addSubviews for clues,answers,score and textField

--- 100_days/project8/project8/ViewController.swift
+++ 100_days/project8/project8/ViewController.swift
@@ -34,7 +34,7 @@ class ViewController: UIViewController {
         view.addSubview(scoreLabel)
         
         cluesLabel = UILabel()
-        cluesLabel.font = UIFont.systemFont(ofSize: 20)
+        cluesLabel.font = UIFont.systemFont(ofSize: 16)
         cluesLabel.text = "CLUES"
         cluesLabel.numberOfLines = 0
         cluesLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -42,24 +42,36 @@ class ViewController: UIViewController {
         
         answersLabel = UILabel()
         answersLabel.translatesAutoresizingMaskIntoConstraints = false
-        answersLabel.font = UIFont.systemFont(ofSize: 20)
+        answersLabel.font = UIFont.systemFont(ofSize: 16)
         answersLabel.numberOfLines = 0
         answersLabel.text = "ANSWERS"
         answersLabel.textAlignment = .right
         view.addSubview(answersLabel)
         
+        currentAnswer = UITextField()
+        currentAnswer.translatesAutoresizingMaskIntoConstraints = false
+        currentAnswer.textAlignment = .center
+        currentAnswer.font = UIFont.systemFont(ofSize: 20)
+        currentAnswer.placeholder = "Tap letters to guess"
+        currentAnswer.isUserInteractionEnabled = false
+        view.addSubview(currentAnswer)
+        
         NSLayoutConstraint.activate([
             scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
             scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
-            cluesLabel.topAnchor.constraint(equalTo: scoreLabel.layoutMarginsGuide.bottomAnchor),
+            cluesLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor),
             cluesLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 20),
             cluesLabel.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.2),
-            answersLabel.topAnchor.constraint(equalTo: scoreLabel.layoutMarginsGuide.bottomAnchor),
+            answersLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor),
             answersLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: -20),
             answersLabel.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.3),
-            answersLabel.heightAnchor.constraint(equalTo: cluesLabel.layoutMarginsGuide.heightAnchor)
-            
+            answersLabel.heightAnchor.constraint(equalTo: cluesLabel.heightAnchor),
+            currentAnswer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            currentAnswer.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.8),
+            currentAnswer.topAnchor.constraint(equalTo: cluesLabel.bottomAnchor, constant: 50),
         ])
+        answersLabel.backgroundColor = .red
+        cluesLabel.backgroundColor = .green
     }
 
 }
