734ec58f696abe7e003ff4a3061be7507d2c4925
Viktor Rubenko <vrubenko@gmail.com>
2021-03-01 22:18:33 +0300

    add subview buttonsView with 20 lettersButtons

--- 100_days/project8/project8/ViewController.swift
+++ 100_days/project8/project8/ViewController.swift
@@ -12,15 +12,36 @@ class ViewController: UIViewController {
     var currentAnswer: UITextField!
     var submitButton: UIButton!
     var clearButton: UIButton!
-    var cluesButtons = [UIButton]()
+    var lettersButtons = [UIButton]()
     var cluesLabel: UILabel!
     var answersLabel: UILabel!
     var scoreLabel: UILabel!
+    var buttonsView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        let width = Int(buttonsView.bounds.width / 4)
+        let height = 45
         
+        for row in 0...4 {
+            for col in 0...3 {
+                let letterButton = UIButton(type: .system)
+                letterButton.titleLabel?.font = UIFont.systemFont(ofSize: 25)
+                letterButton.setTitle("WWW", for: .normal)
+                
+                let frame = CGRect(x: col * width, y: row * height, width: width, height: height)
+                letterButton.frame = frame
+                
+                buttonsView.addSubview(letterButton)
+                
+                lettersButtons.append(letterButton)
+            }
+        }
     }
     
     override func loadView() {
@@ -66,6 +87,10 @@ class ViewController: UIViewController {
         clearButton.setTitle("CLEAR", for: .normal)
         view.addSubview(clearButton)
         
+        buttonsView = UIView()
+        buttonsView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(buttonsView)
+        
         NSLayoutConstraint.activate([
             scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
             scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
@@ -79,15 +104,23 @@ class ViewController: UIViewController {
             currentAnswer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             currentAnswer.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.8),
             currentAnswer.topAnchor.constraint(equalTo: cluesLabel.bottomAnchor, constant: 50),
-            submitButton.topAnchor.constraint(equalTo: currentAnswer.bottomAnchor, constant: 10),
+            submitButton.topAnchor.constraint(equalTo: currentAnswer.bottomAnchor, constant: 20),
             submitButton.leadingAnchor.constraint(equalTo: currentAnswer.leadingAnchor, constant: 10),
             submitButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.2),
             clearButton.topAnchor.constraint(equalTo: submitButton.topAnchor),
             clearButton.widthAnchor.constraint(equalTo: submitButton.widthAnchor),
             clearButton.trailingAnchor.constraint(equalTo: currentAnswer.trailingAnchor, constant: -10),
+            buttonsView.topAnchor.constraint(equalTo: submitButton.bottomAnchor, constant: 50),
+            buttonsView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 10),
+            buttonsView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: -10),
+            buttonsView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: 10),
         ])
-        answersLabel.backgroundColor = .red
-        cluesLabel.backgroundColor = .green
+        
+        
+        cluesLabel.setContentHuggingPriority(UILayoutPriority(1), for: .vertical)
+        answersLabel.setContentHuggingPriority(UILayoutPriority(1), for: .vertical)
+        // 1 - >stretch verticaly
+        
     }
 
 }
