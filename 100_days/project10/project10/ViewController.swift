e944fc3c061482082574bb7455a3d4a1b7275639
Viktor Rubenko <vrubenko@gmail.com>
2021-03-03 22:30:27 +0300

    add 10 cells for UICollection

--- 100_days/project10/project10/ViewController.swift
+++ 100_days/project10/project10/ViewController.swift
@@ -9,5 +9,20 @@ import UIKit
 
 class ViewController: UICollectionViewController{
     
+    override func viewDidLoad() {
+        title = "Names to Faces"
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell")
+        }
+        cell.name.text = "1"
+        return cell
+    }
 }
 
