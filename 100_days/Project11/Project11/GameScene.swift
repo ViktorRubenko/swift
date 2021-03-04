0b41d66ac627cbcdaf355d756706c91adbb57d8c
Viktor Rubenko <vrubenko@gmail.com>
2021-03-04 22:27:45 +0300

    add background, rect on touch and SKPhysics

--- 100_days/Project11/Project11/GameScene.swift
+++ 100_days/Project11/Project11/GameScene.swift
@@ -11,11 +11,22 @@ import GameplayKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
-        
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 0, y: 0)
+        background.blendMode = .replace
+        background.zPosition = -1
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        addChild(background)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            let box = SKSpriteNode(color: UIColor.red, size: CGSize(width: 64, height: 64))
+            box.position = location
+            box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
+            addChild(box)
+        }
     }
     
 }
