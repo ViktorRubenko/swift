4761d79b6bcd9b0bf575adfdf4f35914b32f9396
Viktor Rubenko <vrubenko@gmail.com>
2021-03-04 21:32:37 +0300

    init

---
+++ 100_days/Project11/Project11/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  Project11
+//
+//  Created by Victor Rubenko on 04.03.2021.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+}
