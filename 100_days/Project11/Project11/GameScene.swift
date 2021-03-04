3d3fc824ffb022489e15778df72689dd4b699563
Viktor Rubenko <vrubenko@gmail.com>
2021-03-04 22:53:15 +0300

    add Bouncer

--- 100_days/Project11/Project11/GameScene.swift
+++ 100_days/Project11/Project11/GameScene.swift
@@ -17,15 +17,30 @@ class GameScene: SKScene {
         background.zPosition = -1
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         addChild(background)
+        
+        addBouncer(at: CGPoint(x: 0, y: 0))
+        addBouncer(at: CGPoint(x: 230, y:0))
+        addBouncer(at: CGPoint(x: -230, y:0))
+        addBouncer(at: CGPoint(x: 460, y:0))
+        addBouncer(at: CGPoint(x: -460, y: 0))
+    }
+    
+    func addBouncer(at position: CGPoint) {
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = position
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2.0)
+        bouncer.physicsBody?.isDynamic = false
+        addChild(bouncer)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
             let location = touch.location(in: self)
-            let box = SKSpriteNode(color: UIColor.red, size: CGSize(width: 64, height: 64))
-            box.position = location
-            box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
-            addChild(box)
+            let ball = SKSpriteNode(imageNamed: "ballRed")
+            ball.position = location
+            ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+            ball.physicsBody?.restitution = 0.4
+            addChild(ball)
         }
     }
     
