3f15d75aabdd90386012ecc8abf029c722d5831b
Viktor Rubenko <vrubenko@gmail.com>
2021-03-11 22:15:11 +0300

    add animations

--- 100_days/project15/project15/ViewController.swift
+++ 100_days/project15/project15/ViewController.swift
@@ -26,6 +26,8 @@ class ViewController: UIViewController {
         UIView.animate(
             withDuration: 1,
             delay: 0,
+            usingSpringWithDamping: 0.5,
+            initialSpringVelocity: 5,
             options: [],
             animations: {
                 switch self.currentAnimation {
@@ -34,7 +36,19 @@ class ViewController: UIViewController {
                 case 1:
                     self.imageView.transform = .identity
                 case 2:
-                    self.imageView.transform = CGAffineTransform(translationX: -256, y: -256)
+                    self.imageView.transform = CGAffineTransform(translationX: -100, y: -100)
+                case 3:
+                    self.imageView.transform = .identity
+                case 4:
+                    self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+                case 5:
+                    self.imageView.transform = .identity
+                case 6:
+                    self.imageView.alpha = 0.1
+                    self.imageView.backgroundColor = .green
+                case 7:
+                    self.imageView.alpha = 1
+                    self.imageView.backgroundColor = .clear
                 default:
                     break
                 }
